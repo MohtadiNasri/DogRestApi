243b2d941b8d27440895ba43ad30da1aea117b83
nasrimohtadi@gmail.com <Awkward@009>
2020-03-22 18:12:17 +0100

    Initial commit

---
+++ src/test/java/com/springboot/DogRestApi/DogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.DogRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
